cc562bad1ebe9250f0a088912880bbaf33e0b2bd
Jack <admin@screendead.com>
2022-06-03 13:22:05 +0100

    Init THREE.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Box } from './models/box';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
