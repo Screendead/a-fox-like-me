64a852ec63bc4019ba1503a0af10a4d89f6bdd11
Jack <admin@screendead.com>
2022-06-04 19:14:12 +0100

    Allow user to specify ID, then display proximity of foxes to that ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
 import './App.css';
-import { Canvas } from '@react-three/fiber';
-import { Box } from './models/box';
 import { foximity } from './models/foximity';
 import foxesImport from './models/foxes.json';
 import { Fox } from './models/fox/Fox';
-
-foximity(foxesImport.foxes[1] as Fox).forEach(result => {
-  console.log(`${result.fox.token_id}. "${result.fox.name}" proximity: ${result.proximityPercentage}%`);
-});
+import { useState } from 'react';
 
 function App() {
+  const [tokenID, setTokenID] = useState('');
+  const [fox, setFox] = useState<Fox | null>(null);
+
+  const foxes = foxesImport.foxes.map(fox => fox as Fox);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Canvas>
+        {/* <Canvas>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
           <Box position={[-1.2, 0, 0]} />
           <Box position={[1.2, 0, 0]} />
-        </Canvas>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </Canvas> */}
+        {!fox && <>
+          <p>
+            Enter your Fox's token ID and click the button to see how close your
+            Fox is to other Foxes.
+          </p>
+          <input type="number" value={tokenID} onChange={(e) => setTokenID(e.target.value)} />
+          <button onClick={() => {
+            setFox(foxes.find(fox => fox.token_id === tokenID) || null);
+          }}>Submit</button>
+        </>}
+        {fox && <>
+          <p>
+            Your Fox is "{foxes.find(fox => fox.token_id === tokenID)?.name ?? 'unknown'}"
+          </p>
+          <p>
+            Similar foxes:
+          </p>
+          <ol className='foximities'>
+            {foximity(fox).map(result => <li key={result.fox.token_id}>
+              {result.fox.token_id}. {result.fox.name} ({result.proximityPercentage}%)
+            </li>)}
+          </ol>
+        </>}
       </header>
     </div>
   );
