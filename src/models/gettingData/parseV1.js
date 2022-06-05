9c24517876c104a86d0ffdbdb50e41b819d64950
mrpowergage <mrpowergage@gmail.com>
2022-06-05 20:45:47 +0100

    parsingv1

--- src/models/gettingData/parseV1.js
+++ src/models/gettingData/parseV1.js
@@ -1,17 +1,19 @@
-import fetch from 'node-fetch'
-// import fs from 'node:fs/promises'
+import fs from 'node:fs/promises'
+import inputArray from "./V1FoxesOSData.json"
 
 async function getItem (tokenNum){
-let response = await fetch ("https://opensea.mypinata.cloud/ipfs/QmTM2banPba5x8scA9V6n7KgCbqjjAuarHDXuSboPPWHGh/metadata/" + tokenNum + ".json")
-return await response.json()
+
 }
 
 async function loopFoxes (loopTimes){
     let foxArray = []
+    console.log(inputArray);
 for (let i = 1; i < loopTimes; i++) { //1300 Tokens
-    foxArray.push(await getItem (i))
+    // foxArray.push(await getItem (i))
+
+    console.log(i);
     if (foxArray.length === loopTimes-1) {
-        // fs.writeFile("./data.json",JSON.stringify(foxArray))
+        // fs.writeFile("./parsedV1data.json",JSON.stringify(foxArray))
         // return foxArray
     }
     
@@ -20,7 +22,8 @@ for (let i = 1; i < loopTimes; i++) { //1300 Tokens
 }
 
 
-
-loopFoxes (1300)
+loopFoxes (10)//112
 // console.log(foxArray);
 // fs.writeFile("./data.json",JSON.stringify(foxArray))
+
+
