87fff81a87f15ea149d1dbea2cb0f27a893ae19b
Jack <admin@screendead.com>
2022-06-05 15:48:48 +0100

    Merge dumb UI in

--- src/routes/a-fox-like-me/AFoxLikeMe.tsx
+++ src/routes/a-fox-like-me/AFoxLikeMe.tsx
@@ -43,8 +43,25 @@ export function AFoxLikeMe() {
           {fox && <FoxPlanet position={[0, 0, 0]} fox={fox} />}
         </Canvas>
       </div>
-      <div className="experience-title">A Fox Like Me</div>
-      <div className="experience-fox-name">{fox?.name}</div>
+      <div className="display">
+        <div className="internalDisplay">
+          <div className="heading">
+            <img className="pfp" src="https://lh3.googleusercontent.com/64GLMX85SvN6lehNLg45wePcrBweBlgaRHe-LQh4XXcvdiy8bIGPfiSBw6ZBAbQpuN51ejH0-S2JMvDa9Ht-NFkHdmWvNiY-oAUx=w600" alt="pfp" />
+            <span>
+              <h2 className="component-title">LAGUNA LACUNA</h2>
+              <h3 className="component-subtitle">#417</h3>
+            </span>
+          </div>
+          <p>Philosophy: Stoic</p>
+          <p>Speicies: Jester</p>
+          <p>Virtue: Knack for accents</p>
+          <p>Virtue: Good at swearing</p>
+          <p>Virtue: Makes an increadible curry</p>
+          <p>Baggage: Sanctimonious</p>
+          <p>Baggage: Bad Speller</p>
+          <p>Baggage: Always Late</p>
+        </div>
+      </div>
     </>
   );
 }
