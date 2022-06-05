83034d7f28cc78abb859e5dc23037a82898fea8b
Jack <admin@screendead.com>
2022-06-05 19:52:31 +0100

    Fix random image texture rotation on load

--- src/routes/a-fox-like-me/AFoxLikeMe.tsx
+++ src/routes/a-fox-like-me/AFoxLikeMe.tsx
@@ -1,6 +1,6 @@
 import "./AFoxLikeMe.scss";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Fox } from "../../models/fox/Fox";
 import { foximity } from "../../models/foximity";
 import { Canvas } from "@react-three/fiber";
@@ -44,7 +44,7 @@ export function AFoxLikeMe() {
           {fox && <FoxPlanet position={[0, 0, 0]} fox={fox} />}
         </Canvas>
       </div>
-      {fox && <div className="display">
+      {fox && <div className="display top-left">
         <div className="internalDisplay">
           <div className="heading">
             <img className="pfp" src={fox.image.replace('ipfs://', 'https://ipfs.io/ipfs/')} alt="pfp" />
@@ -54,39 +54,71 @@ export function AFoxLikeMe() {
             </span>
           </div>
           <ul>
-            {fox.attributes.map(a => {
-              return (<li key={a.trait_type}>
+            {fox.attributes.map((a, i) => {
+              return (<li key={`fox-${fox.tokenId}-attr-${i}`}>
                 <span className="trait-name">{a.trait_type}</span>: {a.value}
               </li>);
             })}
           </ul>
         </div>
       </div>}
-      <div className="experience-container">
-        <div className="display">
-          <div className="internalDisplay">
-            <div className="heading">
-              <h2 className="component-title">DISTANT FOXES</h2>
-            </div>
-            <table id="dFoxes">
-              <thead>
-                <tr>
-                  <th>Token ID</th>
-                  <th>Name</th>
-                  <th>Distance</th>
-                </tr>
-              </thead>
-              <tbody>
-                {foximities.map(f => (
-                  <tr key={f.fox.tokenId}>
-                    <td>{f.fox.tokenId}</td>
-                    <td>{f.fox.name}</td>
-                    <td>{f.proximityPercentage}%</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+      <div className="display top-right">
+        <div className="internalDisplay">
+          <div className="heading">
+            <h2 className="component-title">KINDRED FOXES</h2>
           </div>
+          <table className="cFoxes">
+            <thead>
+              <tr>
+                <th>Fox ID</th>
+                <th>Name</th>
+                <th>Similarity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {foximities
+                .slice(0, 10)
+                .map(f => (<tr key={f.fox.tokenId}>
+                    <td>{f.fox.tokenId}</td>
+                    <td>
+                      <Link className="fox-sublink" to={`/find/${f.fox.tokenId}`}>
+                        {f.fox.name}
+                      </Link>
+                    </td>
+                    <td>{f.proximityPercentage}%</td>
+                  </tr>))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="display bottom-right">
+        <div className="internalDisplay">
+          <div className="heading">
+            <h2 className="component-title">DISTANT FOXES</h2>
+          </div>
+          <table className="dFoxes">
+            <thead>
+              <tr>
+                <th>Fox ID</th>
+                <th>Name</th>
+                <th>Similarity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {[...foximities]
+                .sort((a, b) => a.proximityPercentage - b.proximityPercentage)
+                .slice(0, 10)
+                .map(f => (<tr key={f.fox.tokenId}>
+                    <td>{f.fox.tokenId}</td>
+                    <td>
+                      <Link className="fox-sublink" to={`/find/${f.fox.tokenId}`}>
+                        {f.fox.name}
+                      </Link>
+                    </td>
+                    <td>{f.proximityPercentage}%</td>
+                  </tr>))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
