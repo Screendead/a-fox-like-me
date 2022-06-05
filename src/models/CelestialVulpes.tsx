ec53f03e7b4c6a50fb541c563e4345be448f6395
Jack <admin@screendead.com>
2022-06-05 14:38:35 +0100

    Update CelestialVulpes to remove linting issues

--- src/models/CelestialVulpes.tsx
+++ src/models/CelestialVulpes.tsx
@@ -1,24 +1,63 @@
-import { useState } from 'react';
+import * as THREE from 'three';
+import { useEffect, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Foximity } from './fox/Foximity';
-import { FoxPlanet } from './FoxPlanet';
+import { FoxPlanet } from './fox-planet/FoxPlanet';
+
 
 export function CelestialVulpes(props: { foxes: Foximity[] }) {
+  const [rings, setRings] = useState<Foximity[][]>();
+  const [minProxmity, setMinProxmity] = useState(0);
+  const [maxProxmity, setMaxProxmity] = useState(0);
   const [foxPlanets, setFoxPlanets] = useState<JSX.Element[]>([]);
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    let rings: Foximity[][] = [];
+    let _min = 100;
+    let _max = 0;
+    for (let fox of props.foxes) {
+      rings[fox.proximity] = rings[fox.proximity] || [];
+      rings[fox.proximity].push(fox);
+
+      if (fox.proximityPercentage < _min) _min = fox.proximityPercentage;
+      if (fox.proximityPercentage > _max) _max = fox.proximityPercentage;
+    }
+    setRings(rings);
+    setMinProxmity(_min);
+    setMaxProxmity(_max);
+  }, [props.foxes]);
 
   useFrame(() => {
-    let _cv: JSX.Element[] = [];
-    for (let i = 0; i < props.foxes.length; i++) {
-      // Get x, y in circle based on index and length of array
-      let _x = Math.cos(2 * Math.PI * i / props.foxes.length) * 5;
-      let _y = Math.sin(2 * Math.PI * i / props.foxes.length) * 5;
+    if (!rings) return;
 
-      _cv.push(<FoxPlanet key={i} position={[_x, _y, 0]} />);
+    let foxPlanets: JSX.Element[] = [];
+
+    for (let i = 0; i < rings.length; i++) {
+      if (!rings[i]) continue;
+
+      let thisRingProxmity = (i / rings.length) * (maxProxmity - minProxmity) + minProxmity;
+
+      for (let j = 0; j < rings[i].length; j++) {
+        let _f = rings[i][j];
+        let _c = (j / rings[i].length) * Math.PI * 2;
+
+        let position = new THREE.Vector3(
+          Math.cos(_c + rotation / i) * thisRingProxmity,
+          Math.sin(_c + rotation / i) * thisRingProxmity,
+          0,
+        );
+
+        foxPlanets.push(<FoxPlanet key={_f.fox.tokenId} position={position} fox={_f.fox} />);
+      }
     }
-    setFoxPlanets(_cv);
+    setFoxPlanets(foxPlanets);
+    setRotation(rotation + 0.01);
   });
 
   return (
-    <group>{foxPlanets}</group>
+    <>
+      <group>{foxPlanets}</group>
+    </>
   );
 }
