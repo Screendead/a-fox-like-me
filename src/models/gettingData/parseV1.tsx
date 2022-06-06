df541511f3cce94fa7661d2e13d589a3846f582e
mrpowergage <mrpowergage@gmail.com>
2022-06-06 08:19:01 +0100

    done

--- src/models/gettingData/parseV1.tsx
+++ src/models/gettingData/parseV1.tsx
@@ -3,6 +3,9 @@ import inputArray from "./V1FoxesOSData.json"
 
 export async function runMe(): Promise<string> {
   console.log('Handling V1 Foxes...');
+  let FoxJson = {}
+  let foxArray: typeof FoxJson[] = [];
+
 
   function constructItem(i: any) {
     let url = inputArray[i].image_preview_url
@@ -10,31 +13,46 @@ export async function runMe(): Promise<string> {
     let newUrl = url.replace(/s\d+$/, 'w=s256');
     console.log('newUrl', newUrl);
     let traits = inputArray[i].traits
-// delete thisTrait.order;
-// delete thisTrait.order;
     // console.log(traits);
-    let FoxJson = {
-      "image": newUrl,
-      "tokenId": "G"+ inputArray[i].token_id.substring(0, 5),
-      "description": inputArray[i].description,
-      "name": inputArray[i].name,
-      "attributes": traits
+
+    //     for (let i = 1; i < traits.length; i++) {
+    // let thisTrait = traits[i];
+    // delete thisTrait.order;
+    //     }
+    let traitstructure = {}
+    let cleanTraits: typeof traitstructure[] = [];
+    
+    for (let i = 1; i < traits.length; i++) {
+      let thisTrait = traits[i];
+
+      const { order,display_type,max_value,trait_count, ...cleanTrait } = thisTrait;
+      // delete thisTrait.order;
+      cleanTraits.push(cleanTrait)
     }
-    return FoxJson
+      FoxJson = {
+        "image": newUrl,
+        "tokenId": "G" + inputArray[i].token_id.substring(0, 5),
+        "description": inputArray[i].description,
+        "name": inputArray[i].name,
+        "attributes": cleanTraits
+      }
+      return FoxJson
+    
   }
 
+
   async function loopFoxes(loopTimes: any) {
-    let foxArray = []
+
     console.log(inputArray);
 
-    for (let i = 1; i < loopTimes; i++) { // 1300 Tokens
+    for (let i = 0; i < loopTimes; i++) { // 1300 Tokens
       foxArray.push(await constructItem(i))
 
       console.log(i);
       if (foxArray.length === loopTimes - 1) {
-      // fs.writeFile("./parsedV1data.json",JSON.stringify(foxArray))
-      console.log(foxArray);
-
+        // fs.writeFile("./parsedV1data.json",JSON.stringify(foxArray))
+        console.log(foxArray);
+        return foxArray;
         // return foxArray
       }
 
@@ -43,9 +61,13 @@ export async function runMe(): Promise<string> {
   }
 
 
-  // loopFoxes(10) // 112
+  // loopFoxes(3).then(result => {
+
+
+  // got final result
+  // }) // 112
   // console.log(foxArray);
   // fs.writeFile("./data.json",JSON.stringify(foxArray))
-
-  return JSON.stringify([]);
+  return JSON.stringify(await loopFoxes(112));
+  //return await loopFoxes(3)
 }
