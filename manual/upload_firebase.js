7532f3016482c0062a582bf62909b846dea7b9d5
Jack <admin@screendead.com>
2022-06-10 04:40:35 +0100

    Add code to download images from IPFS and upload them to Firebase

---
+++ manual/upload_firebase.js
@@ -0,0 +1,36 @@
+import fs from 'fs/promises';
+import admin from 'firebase-admin';
+import serviceAccount from './a-fox-like-me-firebase-adminsdk-w09a1-5eacc98ceb.json' assert {type: 'json'};
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+async function uploadSingle(file) {
+  await admin.storage().bucket('a-fox-like-me.appspot.com').upload(`./foxes_v1/${file}`, {
+    destination: `foxes/v1/${file}`,
+    metadata: {
+      contentType: 'image/png',
+    },
+  });
+
+  console.log(`Uploaded ${file}`);
+}
+
+
+async function upload() {
+  let promises = [];
+
+  // Get all files in ./foxes
+  let files = await fs.readdir('./foxes_v1');
+  files = files.filter(file => file.endsWith('.png'));
+  for (let i = 0; i < files.length; i++) {
+    promises.push(uploadSingle(files[i]));
+  }
+
+  await Promise.all(promises);
+
+  console.log('Uploaded all files');
+}
+
+
+upload();
