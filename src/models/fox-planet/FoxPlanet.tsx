3ef4d448eeba960c0c36fbb4afec089531632055
Jack <admin@screendead.com>
2022-06-22 18:53:01 +0100

    Negate individual planet rotation

--- src/models/fox-planet/FoxPlanet.tsx
+++ src/models/fox-planet/FoxPlanet.tsx
@@ -53,10 +53,10 @@ export function FoxPlanet(props: JSX.IntrinsicElements['mesh'] & {
 
     let _dr = delta * (props.size ?? 1);
 
-    meshRef.current.rotation.z += _dr;
-    if (props.main) meshRefMain.current.rotation.z += _dr;
+    meshRef.current.rotation.z -= _dr;
+    if (props.main) meshRefMain.current.rotation.z -= _dr;
 
-    texture.rotation -= _dr;
+    texture.rotation += _dr;
 
     if (!orbitPosition) return;
 
